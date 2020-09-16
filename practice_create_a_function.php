7d171cf929018a8e6a7567edca664a0337203cbe
kobayashiukyo <u.k.knua0419@gmail.com>
2020-09-16 18:18:37 +0900

    add_practice_create_a_function.php

---
+++ practice_create_a_function.php
@@ -0,0 +1,42 @@
+<body>
+
+  <?php
+
+    // 関数helloを定義してください
+    function hello(){
+      echo 'Hello, world!';
+    }
+    
+    // 関数helloを呼び出してください
+    hello();
+    
+    echo '<br>';
+    // 関数printRectangleAreaを定義してください
+    function printRectangleArea( $height, $width ){
+      echo $height * $width ;
+    }
+    
+    // 引数を(5, 10)としてprintRectangleAreaを呼び出してください
+    printRectangleArea( 5, 10 )
+    
+  ?>
+
+  <p>// 関数helloを定義してください
+    function hello(){
+      echo 'Hello, world!';
+    }
+    
+    // 関数helloを呼び出してください
+    hello();
+    
+    echo '<br>';
+    // 関数printRectangleAreaを定義してください
+    function printRectangleArea( $height, $width ){
+      echo $height * $width ;
+    }
+    
+    // 引数を(5, 10)としてprintRectangleAreaを呼び出してください
+    printRectangleArea( 5, 10 )</p>
+
+</body>
+</html>
