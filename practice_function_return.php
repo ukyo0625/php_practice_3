ebcb1277f88595873d697aa516cc9d24be431fcd
kobayashiukyo <u.k.knua0419@gmail.com>
2020-09-16 18:28:06 +0900

    add_practice_function_return.php

---
+++ practice_function_return.php
@@ -0,0 +1,36 @@
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Progate</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+
+  <?php
+
+    // 関数getCircleAreaを定義してください
+    function getCircleArea($radius){
+      return $radius * $radius * 3 ;
+    }
+    
+    // 関数getCircleAreaを呼び出して、戻り値を変数$circleAreaに代入してください
+    $circleArea = getCircleArea(5);
+    
+    // $circleAreaをechoしてください
+    echo $circleArea
+    
+  ?>
+
+  <p>// 関数getCircleAreaを定義してください
+    function getCircleArea($radius){
+      return $radius * $radius * 3 ;
+    }
+    
+    // 関数getCircleAreaを呼び出して、戻り値を変数$circleAreaに代入してください
+    $circleArea = getCircleArea(5);
+    
+    // $circleAreaをechoしてください
+    echo $circleArea
+  </p>
+</body>
+</html>
