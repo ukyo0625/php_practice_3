15704af919d1cecee06f967179a6efd7af107941
kobayashiukyo <u.k.knua0419@gmail.com>
2020-09-16 17:58:44 +0900

    add_function.php

---
+++ practice_function.php
@@ -0,0 +1,44 @@
+</head>
+<body>
+
+  <?php
+
+    $str = 'progate';
+
+    // strlenを用いて$strの長さをechoしてください
+    echo strlen($str);
+    
+    echo '<br>';
+    
+    $array = array('HTML', 'CSS', 'PHP');
+
+    // countを用いて$arrayの要素数をechoしてください
+    echo count($array);
+    
+    echo '<br>';
+    
+    // randを用いて10から15までのランダムな数字をechoしてください
+    echo rand( 10, 15);
+    
+  ?>
+
+  <p>$str = 'progate';
+
+    // strlenを用いて$strの長さをechoしてください
+    echo strlen($str);
+    
+    echo '<br>';
+    
+    $array = array('HTML', 'CSS', 'PHP');
+
+    // countを用いて$arrayの要素数をechoしてください
+    echo count($array);
+    
+    echo '<br>';
+    
+    // randを用いて10から15までのランダムな数字をechoしてください
+    echo rand( 10, 15);
+</p>
+
+</body>
+</html>
