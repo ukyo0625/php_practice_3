2123a7b78c3726d9ec8bc03ae9559ceb6d2f9f32
kobayashiukyo <u.k.knua0419@gmail.com>
2020-09-20 21:08:08 +0900

    fix_title

--- practice_create_a_function.php
+++ practice_create_a_function.php
@@ -1,41 +1,32 @@
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Practice</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
 <body>
 
   <?php
-
-    // 関数helloを定義してください
     function hello(){
       echo 'Hello, world!';
     }
-    
-    // 関数helloを呼び出してください
     hello();
-    
     echo '<br>';
-    // 関数printRectangleAreaを定義してください
     function printRectangleArea( $height, $width ){
       echo $height * $width ;
     }
-    
-    // 引数を(5, 10)としてprintRectangleAreaを呼び出してください
     printRectangleArea( 5, 10 )
-    
   ?>
 
-  <p>// 関数helloを定義してください
+  <p>
     function hello(){
       echo 'Hello, world!';
     }
-    
-    // 関数helloを呼び出してください
     hello();
-    
     echo '<br>';
-    // 関数printRectangleAreaを定義してください
     function printRectangleArea( $height, $width ){
       echo $height * $width ;
     }
-    
-    // 引数を(5, 10)としてprintRectangleAreaを呼び出してください
     printRectangleArea( 5, 10 )</p>
 
 </body>
--- practice_function.php
+++ practice_function.php
@@ -1,42 +1,28 @@
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Practice</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
 </head>
 <body>
 
   <?php
-
-    $str = 'progate';
-
-    // strlenを用いて$strの長さをechoしてください
+    $str = 'pro';
     echo strlen($str);
-    
     echo '<br>';
-    
     $array = array('HTML', 'CSS', 'PHP');
-
-    // countを用いて$arrayの要素数をechoしてください
     echo count($array);
-    
     echo '<br>';
-    
-    // randを用いて10から15までのランダムな数字をechoしてください
     echo rand( 10, 15);
-    
   ?>
 
-  <p>$str = 'progate';
-
-    // strlenを用いて$strの長さをechoしてください
+  <p>
+    $str = 'pro';
     echo strlen($str);
-    
     echo '<br>';
-    
     $array = array('HTML', 'CSS', 'PHP');
-
-    // countを用いて$arrayの要素数をechoしてください
     echo count($array);
-    
     echo '<br>';
-    
-    // randを用いて10から15までのランダムな数字をechoしてください
     echo rand( 10, 15);
 </p>
 
